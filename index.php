657317e46d25234d6bc980d7eb37166d27f74663
alexanderweb1 <alexanderbt@gmail.com>
2026-05-22 10:27:02 -0500

    agregamos ingresos.php, modelo y controlador

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 //llamar a todos los controladores
 require_once "controladores/ctrlPrincipal.php";
 require_once "controladores/ctrlUsurios.php";
-require_once "controladores/ctrlPersona.php";
+#require_once "controladores/ctrlPersona.php";
 
 
 //llamar a todos los modelos
 require_once "modelos/mdlUsuario.php";
-require_once "modelos/mdlPersona.php";
- 
+#require_once "modelos/mdlPersona.php";
+
 $objPrincipal = new Principal();
 $objPrincipal->ctrlPrincipal();
